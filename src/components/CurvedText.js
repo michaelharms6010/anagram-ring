5fec8b003ffc4377a4be16e7964eb55b2ee46426
Michael Harms <michael.harms6010@gmail.com>
2023-09-12 11:58:12 -0500

    clean indents

--- src/components/CurvedText.js
+++ src/components/CurvedText.js
@@ -23,11 +23,11 @@ export default ({initialText}) => {
   }, [text])
 
   const getCirclePosition = (angle, radius) => {
-      const radian = (angle * Math.PI) / 180;
-      const x = radius + (radius * Math.sin(radian));
-      const y = radius + (radius * Math.cos(radian));
-      
-      return { x, y };
+    const radian = (angle * Math.PI) / 180;
+    const x = radius + (radius * Math.sin(radian));
+    const y = radius + (radius * Math.cos(radian));
+    
+    return { x, y };
   }
 
   const handleChange = e => {
@@ -35,44 +35,45 @@ export default ({initialText}) => {
       setText(e.target.value.replace(/[^a-z]/ig, ""))
     }
   }
-      return (
-        <div className="curved-text-container">
-      
+  
+  return (
+    <div className="curved-text-container">
+  
 
-          <div style={{ marginBottom: 10, position: 'relative', width: 2*radius, height: 2*radius }}>
-            {Array.from(text).map((char, index) => {
-              const angle = angleStep * index;
-              const position = getCirclePosition(angle, radius);
+      <div style={{ marginBottom: 10, position: 'relative', width: 2*radius, height: 2*radius }}>
+        {Array.from(text).map((char, index) => {
+          const angle = angleStep * index;
+          const position = getCirclePosition(angle, radius);
 
-              return (
-                <div
-                  key={index}
-                  style={{
-                      position: 'absolute',
-                      left: `${position.x}px`,
-                      top: `${position.y}px`,
-                      fontSize: 24
-                  }}
-                >
-                  {char.toUpperCase()}
-                </div>
-              );
-            })}
-          </div>
-          <TextField
-            style={{marginTop: 50}}
-            placeholder="Enter letters to anagram" 
-            value={text} 
-            onChange={handleChange} 
-          />
-          <Controls 
-            handleShuffle={handleShuffle}
-            handleReset={handleReset}
-            text={text}
+          return (
+            <div
+              key={index}
+              style={{
+                  position: 'absolute',
+                  left: `${position.x}px`,
+                  top: `${position.y}px`,
+                  fontSize: 24
+              }}
+            >
+              {char.toUpperCase()}
+            </div>
+          );
+        })}
+      </div>
+      <TextField
+        style={{marginTop: 50}}
+        placeholder="Enter letters to anagram" 
+        value={text} 
+        onChange={handleChange} 
+      />
+      <Controls 
+        handleShuffle={handleShuffle}
+        handleReset={handleReset}
+        text={text}
 
-          />
-        </div>
-      );
+      />
+    </div>
+  );
   
 }
 
